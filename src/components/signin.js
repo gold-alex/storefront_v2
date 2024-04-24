5564d5397083822a93505117324536b5b2dc5149
Sammy Mathias Wanyama <99073324+wanyama413@users.noreply.github.com>
2024-04-25 00:01:13 +0300

    event listener

--- src/components/signin.js
+++ src/components/signin.js
@@ -1,29 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import { SignIn, SignedOut, SignUpButton, SignInButton } from "@clerk/nextjs";
 
 export default function Example() {
   const router = useRouter();
+  const containerRef = useRef(null);
 
-  const handleClick = () => {
-    // If the current pathname is /login, replace it with /
-    if (router.pathname === "/login") {
-      router.push("/");
+  const handleClick = (event) => {
+    // Check if the click happened outside the container
+    if (containerRef.current && !containerRef.current.contains(event.target)) {
+      // If the current pathname is /login, replace it with /
+      if (router.pathname === "/login") {
+        router.push("/");
+      }
     }
   };
 
   useEffect(() => {
-    // Add event listener to listen for clicks on the component
-    const handleClickOutside = () => {
-      handleClick();
-    };
-
-    // Attach event listener
-    document.addEventListener("click", handleClickOutside);
+    // Add event listener to listen for clicks outside the container
+    document.addEventListener("click", handleClick);
 
     // Detach event listener on component unmount
     return () => {
-      document.removeEventListener("click", handleClickOutside);
+      document.removeEventListener("click", handleClick);
     };
   }, [router]);
 
@@ -33,7 +32,10 @@ export default function Example() {
       <div className="absolute inset-0 blur-md bg-white"></div>
 
       {/* Container for the SignIn component */}
-      <div className="flex justify-center items-center h-full">
+      <div
+        ref={containerRef}
+        className="flex justify-center items-center h-full"
+      >
         <SignIn routing="hash">
           <SignUpButton />
         </SignIn>
