7ec8e427fc423b3b531749f29f9346a17fe048df
Sammy Mathias Wanyama <99073324+wanyama413@users.noreply.github.com>
2024-04-25 09:34:58 +0300

    public routes

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,5 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  matcher: [
-    "/((?!.+.[w]+$|_next).*)",
-    "/",
-    "/(api|trpc)(.*)",
-    "/api/webhooks(.*)",
-  ],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
