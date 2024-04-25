6821b4514208686ea3c59bccac4eccd9779c9b46
Sammy Mathias Wanyama <99073324+wanyama413@users.noreply.github.com>
2024-04-26 00:32:45 +0300

    ngrok

---
+++ src/pages/api/webhooks.js
@@ -0,0 +1,54 @@
+// @ts-ignore
+import { prisma } from "../../../lib/db";
+import { Webhook } from "svix";
+import { buffer } from "micro";
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const webhookSecret = process.env.WEBHOOK_SECRET || "";
+
+async function handler(req, res) {
+  const payload = (await buffer(req, res)).toString();
+  const headers = req.headers;
+
+  const wh = new Webhook(secret);
+  let msg;
+  try {
+    msg = wh.verify(payload, headers);
+  } catch (err) {
+    res.status(400).json({});
+  }
+
+  const eventType = evt.type;
+  if (eventType === "user.created" || eventType === "user.updated") {
+    const { id, first_name, last_name, username, phone_number } = evt.data;
+
+    await prisma.comment.upsert({
+      where: { externalId: id },
+      create: {
+        externalId: id,
+        email: first_name,
+        firstName: first_name,
+        lastName: last_name,
+        phone: phone_number,
+
+        username: username,
+      },
+      update: {
+        email: first_name,
+        firstName: first_name,
+        lastName: last_name,
+        phone: phone_number,
+
+        username: username,
+      },
+    });
+  }
+}
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
