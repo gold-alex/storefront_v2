362cf19349a73d695455ddf8a6c6cc6c0e0a7283
Sammy Mathias Wanyama <99073324+wanyama413@users.noreply.github.com>
2024-04-25 13:24:08 +0300

    fin

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,67 +1,69 @@
-import { Webhook } from "svix";
-import { WebhookEvent } from "@clerk/nextjs/server";
-import { NextApiRequest, NextApiResponse } from "next";
-import { buffer } from "micro";
+// @ts-ignore
+import { prisma } from "../../../lib/db";
+import { clerkClient } from "@clerk/nextjs/server";
+import { IncomingHttpHeaders } from "http";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { Webhook, WebhookRequiredHeaders } from "svix";
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
+const webhookSecret = process.env.WEBHOOK_SECRET || "";
+
+async function handler(request: Request) {
+  const payload = await request.json();
+  const headersList = headers();
+  const heads = {
+    "svix-id": headersList.get("svix-id"),
+    "svix-timestamp": headersList.get("svix-timestamp"),
+    "svix-signature": headersList.get("svix-signature"),
+  };
+  const wh = new Webhook(webhookSecret);
+  let evt: Event | null = null;
+
+  try {
+    evt = wh.verify(
+      JSON.stringify(payload),
+      heads as IncomingHttpHeaders & WebhookRequiredHeaders
+    ) as Event;
+  } catch (err) {
+    console.error((err as Error).message);
+    return NextResponse.json({}, { status: 400 });
+  }
+
+  const eventType: EventType = evt.type;
+  if (eventType === "user.created" || eventType === "user.updated") {
+    const { id, first_name, last_name, username, phone_number } = evt.data;
+
+    await prisma.comment.upsert({
+      where: { externalId: id as string },
+      create: {
+        externalId: id as string,
+        email: first_name as string,
+        firstName: first_name as string,
+        lastName: last_name as string,
+        phone: phone_number as string,
+
+        username: username as string,
+      },
+      update: {
+        email: first_name as string,
+        firstName: first_name as string,
+        lastName: last_name as string,
+        phone: phone_number as string,
+
+        username: username as string,
+      },
+    });
+  }
+}
+
+type EventType = "user.created" | "user.updated" | "*";
+
+type Event = {
+  data: Record<string, string | number>;
+  object: "event";
+  type: EventType;
 };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    return res.status(405);
-  }
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
-
-  if (!WEBHOOK_SECRET) {
-    throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
-    );
-  }
-
-  // Get the headers
-  const svix_id = req.headers["svix-id"] as string;
-  const svix_timestamp = req.headers["svix-timestamp"] as string;
-  const svix_signature = req.headers["svix-signature"] as string;
-
-  // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return res.status(400).json({ error: "Error occured -- no svix headers" });
-  }
-
-  console.log("headers", req.headers, svix_id, svix_signature, svix_timestamp);
-  // Get the body
-  const body = (await buffer(req)).toString();
-
-  // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
-
-  let evt: WebhookEvent;
-
-  // Verify the payload with the headers
-  try {
-    evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
-    }) as WebhookEvent;
-  } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return res.status(400).json({ Error: err });
-  }
-
-  // Get the ID and type
-  const { id } = evt.data;
-  const eventType = evt.type;
-
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-  console.log("Webhook body:", body);
-
-  return res.status(200).json({ response: "Success" });
-}
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
