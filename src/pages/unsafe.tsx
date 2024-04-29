8bb7b1aaf13ee81883a8ae28149084d9babad67c
Sammy Mathias Wanyama <99073324+wanyama413@users.noreply.github.com>
2024-04-29 14:47:34 +0300

    formatted  dates

--- src/pages/unsafe.tsx
+++ src/pages/unsafe.tsx
@@ -3,13 +3,14 @@ import { useState } from "react";
 import { useRouter } from "next/router"; // Import the useRouter hook
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
+import moment from "moment";
 export default function UnSafePage() {
   const router = useRouter(); // Initialize the useRouter hook
   const { user } = useUser();
-  const [birthday, setBirthday] = useState(new Date());
+  const [birthda, setBirthda] = useState(new Date());
 
   const updateBirthday = async () => {
+    const birthday = moment.utc(birthda).format("Do MMMM YYYY");
     try {
       await user.update({
         unsafeMetadata: {
@@ -23,15 +24,14 @@ export default function UnSafePage() {
       // Handle error if necessary
     }
   };
-
   console.log(user);
   return (
     <div className="flex justify-center items-center h-[100vh] bg-white">
       <div className="w-[60vh] flex flex-col items-center">
         <DatePicker
           className="text-[#02020A] mb-10"
-          selected={birthday}
-          onChange={(date) => setBirthday(date)}
+          selected={birthda}
+          onChange={(date) => setBirthda(date)}
           peekNextMonth
           showMonthDropdown
           showYearDropdown
