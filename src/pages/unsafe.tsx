f94e86d720838030afbc6cfcd6b869b12565fe4a
Sammy Mathias Wanyama <99073324+wanyama413@users.noreply.github.com>
2024-04-29 08:24:27 +0300

    async

--- src/pages/unsafe.tsx
+++ src/pages/unsafe.tsx
@@ -1,30 +1,11 @@
 import { useUser } from "@clerk/nextjs";
 import { useState } from "react";
-import { useRouter } from "next/router"; // Import the useRouter hook
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
 export default function UnSafePage() {
-  const router = useRouter(); // Initialize the useRouter hook
   const { user } = useUser();
   const [birthday, setBirthday] = useState(new Date());
 
-  const updateBirthday = async () => {
-    try {
-      await user.update({
-        unsafeMetadata: {
-          birthday,
-        },
-      });
-      // Navigate to the home page after updating the birthday
-      router.push("/");
-    } catch (error) {
-      console.error("Error updating birthday:", error);
-      // Handle error if necessary
-    }
-  };
-
-  console.log(user);
   return (
     <div className="flex justify-center items-center h-[100vh] bg-white">
       <div className="w-[60vh] flex flex-col items-center">
@@ -39,8 +20,15 @@ export default function UnSafePage() {
         />
 
         <button
-          className="bg-red-500 p-3 border-none outline-none rounded-xl cursor-pointer"
-          onClick={updateBirthday} // Call the updateBirthday function on click
+          className="bg-red-500 p-3 border-none
+           outline-none rounded-xl cursor-pointer"
+          onClick={() => {
+            user.update({
+              unsafeMetadata: {
+                birthday,
+              },
+            });
+          }}
         >
           Update Birthday
         </button>
